f09bf9e8fb7c14d4393d9fad0cec6f6b5ad3d523
Constantin Gavrilescu <cgavrilescu@enova.com>
2023-02-16 23:08:58 -0600

    Priming jobs

--- bulk.rb
+++ bulk.rb
@@ -16,6 +16,14 @@ jobs = (1..900_000).map { |i| [i, i.to_s]  }
 puts 'Done.'
 puts "Memory used: #{GetProcessMem.new.mb}"
 
+puts "sending a job with perform_async"
+EasyJob.perform_async(jobs[0])
+puts "Memory used: #{GetProcessMem.new.mb}"
+
+puts "sending a 100 jobs with perform_bulk"
+EasyJob.perform_bulk(jobs.first(100))
+puts "Memory used: #{GetProcessMem.new.mb}"
+
 puts 'enqueueing jobs to sidekiq'
 EasyJob.perform_bulk(jobs)
 puts "done enqueuing #{jobs.size} jobs to sidekiq"
@@ -23,6 +31,18 @@ puts "Memory used: #{GetProcessMem.new.mb}"
 
 puts "Running GC"
 GC.start
+puts "Memory used: #{GetProcessMem.new.mb}"
+
+puts 'clearing redis memory'
+Redis.new.flushall
+puts "Memory used: #{GetProcessMem.new.mb}"
+
+puts 'Running in batches with perform_bulk'
+jobs.each_slice(10000) do |smaller_jobs|
+  EasyJob.perform_bulk(smaller_jobs)
+end
+GC.start
+puts 'Done.'
 puts "Memory used: #{GetProcessMem.new.mb}"
 
 puts 'clearing redis memory'
